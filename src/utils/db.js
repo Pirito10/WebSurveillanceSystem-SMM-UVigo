e7f36b9bb5f528ec2dacd733aae9f43b05eabfe0
Pirito10 <aaronriveiro@gmail.com>
2024-12-03 01:23:21 +0100

    changed database library to better-sqlite3

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+const database = require('better-sqlite3');
 const config = require('../../config/default'); // Fichero de configuración
 const createDirectory = require('../utils/utils'); // Fichero de útiles
 
@@ -10,43 +10,36 @@ createDirectory(dataFolder, 'DB');
 // Fichero de la base de datos
 const dbPath = path.join(dataFolder, 'app.db');
 
-// Creamos la conexión a SQLite
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        console.error('[DB] Error connecting to SQLite:', err.message);
-    } else {
-        console.log('[DB] Succesfully connected to SQLite');
-    }
-});
+// Creamos la conexión a la base de datos
+const db = new database(dbPath);
 
-// Creamos las tablas si no existen
-db.serialize(() => {
-    // Tabla de usuarios
-    db.run(`
+// Creamos la tabla de usuarios si no existe
+try {
+    db.prepare(`
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT UNIQUE NOT NULL,
             password TEXT NOT NULL
         )
-    `, (err) => {
-        if (err) {
-            console.error(`[DB] Error while creating 'users' table: ${err.message}`);
-        }
-    });
+    `).run();
+    console.log(`[DB] Loaded 'users' table`);
+} catch (err) {
+    console.error(`[DB] Error while creating 'users' table: ${err.message}`);
+}
 
-    // Tabla de flujos
-    db.run(`
+// Creamos la tabla de flujos si no existe
+try {
+    db.prepare(`
         CREATE TABLE IF NOT EXISTS streams (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER NOT NULL,
-            stream_name TEXT NOT NULL,
-            stream_url TEXT NOT NULL,
+            name TEXT NOT NULL,
+            url TEXT NOT NULL,
             config TEXT,
-            FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+            FOREIGN KEY (user_id) REFERENCES users(id)
         )
-    `, (err) => {
-        if (err) {
-            console.error(`[DB] Error while creating 'streams' table: ${err.message}`);
-        }
-    });
-});
+    `).run();
+    console.log(`[DB] Loaded 'streams' table`);
+} catch (err) {
+    console.error(`[DB] Error while creating 'streams' table: ${err.message}`);
+}
